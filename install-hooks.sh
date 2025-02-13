508330eaf05f0fc28fdfbca890ecb1ef2119d5e7
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-13 14:13:37 -0500

    Update

--- install-hooks.sh
+++ install-hooks.sh
@@ -3,6 +3,7 @@
 # for each file in .hooks directory
 for file in $(ls .hooks); do
   # create a symlink to the file in .git/hooks
+  echo "Creating symlink for $file"
   chmod +x .hooks/$file
   ln -s .hooks/$file .git/hooks/$file
 done
