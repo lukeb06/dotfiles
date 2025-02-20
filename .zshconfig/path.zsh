c05d40a9dc7879b45f9917d518a37c92643c53bf
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-20 12:27:09 -0500

    Update

--- .zshconfig/path.zsh
+++ .zshconfig/path.zsh
@@ -16,3 +16,10 @@ alias h2='$(npm prefix -s)/node_modules/.bin/shopify hydrogen'
 
 export EDITOR="/opt/homebrew/bin/nvim"
 export VISUAL="$EDITOR"
+
+function _gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@ ;}
+
+function gi
+{
+    _gi $(_gi list | sed 's/,/\n/g' | fzf),macos,linux,windows
+}
