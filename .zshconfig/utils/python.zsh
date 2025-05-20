b233f07c8ca0cc58d355dfb54521ba436a65e1de
Luke Barrier <lukebbarrier06@outlook.com>
2025-05-19 20:34:58 -0400

    Update

--- .zshconfig/utils/python.zsh
+++ .zshconfig/utils/python.zsh
@@ -3,7 +3,7 @@ alias venv="source .venv/bin/activate"
 alias cvenv="py -m venv .venv && venv"
 alias pip="py -m pip"
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-export PIPENV_VENV_IN_PROJECT=1
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
+# export PIPENV_VENV_IN_PROJECT=1
