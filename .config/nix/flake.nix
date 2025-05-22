e6f2a046017446ded19d712e3074f6d96bca21fc
Luke Barrier <lukebbarrier06@outlook.com>
2025-05-22 10:23:42 -0400

    Update

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -80,6 +80,10 @@
             home-manager.useUserPackages = true;
             home-manager.users.lukebarrier = ./home.nix;
 
+            home-manager.extraSpecialArgs = {
+                inherit inputs; # This passes the entire 'inputs' set to home.nix
+            };
+
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
--- .config/nix/home.nix
+++ .config/nix/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -43,6 +43,10 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+
+    # ".zshrc".source = ~/dotfiles/.zshrc;
+    ".config/kitty".source = "${inputs.self}/dotfiles/.config/kitty";
+
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
